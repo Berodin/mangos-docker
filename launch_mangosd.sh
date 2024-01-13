3799fc77b11473761b8def4a9a2effaf11e13caf
Markus <1994freeway@gmail.com>
2024-01-13 20:18:12 +0100

    add if condition to check whether file is there and separate it from AHconfig arg

--- launch_mangosd.sh
+++ launch_mangosd.sh
@@ -36,7 +36,10 @@ if [ -f /mangosconf/ahbot.conf ]; then
     echo "/mangosdconf/ahbot.conf is being used"
     AHCONFIG="-a /mangosconf/ahbot.conf"
 else
-    [ ! -f $CONFIGS/ahbot.conf ] && cp $CONFDIR/ahbot.conf.dist $CONFIGS/ahbot.conf && AHCONFIG="-a $CONFIGS/ahbot.conf"
+    if [ ! -f $CONFIGS/ahbot.conf ]; then
+        cp $CONFDIR/ahbot.conf.dist $CONFIGS/ahbot.conf
+    fi
+    AHCONFIG="-a $CONFIGS/ahbot.conf"
 fi
 
 
