368aac1c9d0be03aeb8002c6a29aa7de0316fe9f
Markus <1994freeway@gmail.com>
2024-01-19 10:59:05 +0100

    changes for supervisord

--- launch_mangosd.sh
+++ launch_mangosd.sh
@@ -54,4 +54,4 @@ sed -i 's,WorldDatabaseInfo.*=.*,WorldDatabaseInfo = '"$(echo $WORLD_DATABASE_IN
 sed -i 's,CharacterDatabaseInfo.*=.*,CharacterDatabaseInfo = '"$(echo $CHARACTER_DATABASE_INFO | sed 's/[;&]/\\&/g')"',g' $CONFIGS/mangosd.conf
 sed -i 's,'/server/install/etc/','/etc/mangos/',' $CONFIGS/mangosd.conf
 
-${BINDIR}/mangosd -c $CONFIGS/mangosd.conf ${AHCONFIG}
+#${BINDIR}/mangosd -c $CONFIGS/mangosd.conf ${AHCONFIG}
